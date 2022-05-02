26441c10795b0129dc6dafd542cf8f24aa72e8c7
cfernandez7 <cfernandez@williamhill.us>
2022-05-02 14:30:28 -0400

    Adjust type ascriptions

--- src/main/scala/Services.scala
+++ src/main/scala/Services.scala
@@ -4,17 +4,6 @@ import zio.*
 trait NumService:
   val get: Task[Int]
 
-final case class NumServiceLive(c: ChannelFactory, e: EventLoopGroup) extends NumService:
-  val url = "https://random-num-x5ht4amjia-uc.a.run.app/"
-  val get: Task[Int] =
-    val request =
-      for
-        resp <- Client.request(url)
-        body <- resp.bodyAsString
-      yield body.toInt
-
-    request.provideEnvironment(ZEnvironment(c, e))
-
 object NumService:
   val get: RIO[NumService, Int] = ZIO.serviceWithZIO[NumService](_.get)
 
@@ -26,11 +15,34 @@ object NumService:
       } yield NumServiceLive(c, e)
     )
 
+final case class NumServiceLive(c: ChannelFactory, e: EventLoopGroup) extends NumService:
+  val url = "https://random-num-x5ht4amjia-uc.a.run.app/"
+  val get: Task[Int] =
+    val request =
+      for
+        resp <- Client.request(url)
+        body <- resp.bodyAsString
+      yield body.toInt
+
+    request.provideEnvironment(ZEnvironment(c, e))
+
 trait WordService:
   val get: ZIO[Any, Throwable, String]
 
-case class WordServiceLive(layers: ZLayer[Any, Nothing, ChannelFactory & EventLoopGroup]) extends WordService:
-  val url = "https://random-word-x5ht4amjia-uc.a.run.app/"
+object WordService:
+  val get: ZIO[WordService, Throwable, String] = ZIO.serviceWithZIO[WordService](_.get)
+
+  val live: ZLayer[ChannelFactory & EventLoopGroup, Nothing, WordService] =
+    ZLayer.fromZIO(
+      for {
+        c <- ZIO.service[ChannelFactory]
+        e <- ZIO.service[EventLoopGroup]
+      } yield WordServiceLive(c, e)
+    )
+
+final case class WordServiceLive(c: ChannelFactory, e: EventLoopGroup) extends WordService:
+  private val url = "https://random-word-x5ht4amjia-uc.a.run.app/"
+
   val get: ZIO[Any, Throwable, String] =
     val z =
       for
@@ -38,16 +50,4 @@ case class WordServiceLive(layers: ZLayer[Any, Nothing, ChannelFactory & EventLo
         body <- resp.bodyAsString
       yield body
 
-    z.provideLayer(layers)
-
-object WordService:
-  val get: ZIO[WordService, Throwable, String] = ZIO.serviceWithZIO[WordService](_.get)
-case class WordServiceLive(c: ChannelFactory, e: EventLoopGroup) extends WordService:
-  z.provideEnvironment(ZEnvironment(c, e))
-val live: ZLayer[ChannelFactory & EventLoopGroup, Nothing, WordService] =
-  ZLayer.fromZIO(
-    for {
-      c <- ZIO.service[ChannelFactory]
-      e <- ZIO.service[EventLoopGroup]
-    } yield WordServiceLive(c, e)
-  )
+    z.provideEnvironment(ZEnvironment(c, e))
--- src/main/scala/WebApp.scala
+++ src/main/scala/WebApp.scala
@@ -1,4 +1,4 @@
-import zhttp.http.{Http, Response}
+import zhttp.http.{Http, HttpApp, Response}
 import zhttp.service.{ChannelFactory, EventLoopGroup, Server}
 import zio.{ZIO, ZIOAppDefault, ZLayer}
 
@@ -11,24 +11,21 @@ object WebApp extends ZIOAppDefault:
       words <- ZIO.collectAllPar(reqs)
     yield words.mkString(" ")
 
-  val app =
+  val app: HttpApp[NumService & WordService, Throwable] =
     Http.fromZIO(gibberish.map(Response.text))
 
   def run =
-    val clientLayers = ChannelFactory.auto ++ EventLoopGroup.auto()
-    val layers       = ZLayer.succeed(NumServiceLive(clientLayers)) ++ ZLayer.succeed(WordServiceLive(clientLayers))
-    reqs = Seq.fill(num)(WordService.get)
-  // alternatively rename this to ULayer[A] since it's aliased to ZLayer[Any, Nothing, A]
-  val channelFactory: ZLayer[Any, Nothing, ChannelFactory] = ChannelFactory.auto
-  val eventLoopGroup: ZLayer[Any, Nothing, EventLoopGroup] = EventLoopGroup.auto()
+    // alternatively rename this to ULayer[A] since it's aliased to ZLayer[Any, Nothing, A]
+    val channelFactory: ZLayer[Any, Nothing, ChannelFactory] = ChannelFactory.auto
+    val eventLoopGroup: ZLayer[Any, Nothing, EventLoopGroup] = EventLoopGroup.auto()
 
-  Server
-    .start(8080, app)
-    .provide(
-      channelFactory,
-      eventLoopGroup,
-      NumService.live,
-      WordService.live
-      //ZLayer.Debug.mermaid  // Uncomment this in order to generate Mermaid.JS graphs
-    )
-    .exitCode
+    Server
+      .start(8080, app)
+      .provide(
+        channelFactory,
+        eventLoopGroup,
+        NumService.live,
+        WordService.live
+        //ZLayer.Debug.mermaid  // Uncomment this in order to generate Mermaid.JS graphs
+      )
+      .exitCode
